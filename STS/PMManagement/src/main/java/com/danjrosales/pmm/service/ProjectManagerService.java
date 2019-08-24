2d219c5d1156d0e226c86a0433aea5f081f2149a
DanJRM <dannas@protonmail.com>
2019-08-23 21:02:24 -0500

    Project was updated

--- STS/PMManagement/src/main/java/com/danjrosales/pmm/service/ProjectManagerService.java
+++ STS/PMManagement/src/main/java/com/danjrosales/pmm/service/ProjectManagerService.java
@@ -1,5 +1,48 @@
 package com.danjrosales.pmm.service;
 
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Service;
+
+import com.danjrosales.pmm.entities.ProjectManager;
+
+@Service
+@Path("/projectmanagerservice")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public interface ProjectManagerService {
 
+	@Path("/projectmanagers")
+	@GET
+	public List<ProjectManager> getAll();
+	
+	@Path("/projectmanager/{id}")
+	@GET
+	public ProjectManager getProjectManager(@PathParam(value = "id")int id);
+	
+	@Path("/projectmanager")
+	@POST
+	public ProjectManager createProjectManager(ProjectManager projectManager);
+	
+	@Path("/projectmanager")
+	@PUT
+	public Response updateProjectManager(ProjectManager projectManager);
+	
+	@Path("/projectmanager/{id}")
+	@DELETE
+	public Response deleteProjectManager(@PathParam(value = "id")int id);
+	
 }
